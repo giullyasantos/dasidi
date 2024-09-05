22c4acb8c305ab47c3209e058b6f26281ab4980a
Giullya Santos <souzasantosgiullya@gmail.com>
2024-09-05 08:18:03 -0400

    RSVP pages started + Gifts Paqge Layout done

--- src/pages/gifts.js
+++ src/pages/gifts.js
@@ -2,7 +2,18 @@ import React from 'react';
 import '../App.css'; // Import the CSS file for styling
 
 const Gifts = () => {
-  return <div>Gift Page</div>;
+  return <div className='gifts'>
+    <div>
+      <h1>GIFTS</h1>
+      <p>Your presence at our wedding is the greatest gift we ould ask for. If you wish to honor us with a gift, we have registered for items that will help us start our new life together</p>
+      <div className='registry'>
+        <a href='www.amazon.com'>amazon</a>
+        <a href='www.target.com'>target</a>
+        <a href='www.bedbathandbeyond.com'>bedbathandbeyond</a>
+      </div>
+    </div>
+
+  </div>;
 };
 
 export default Gifts;
--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Background from '../media/movingbackground.mp4'
+import Image from '../media/SaveTheDate.png'
 
 const RSVP = () => {
-  return <div>RSVP Page</div>;
+  const [invitation, setInvitationValue] = useState('');
+  const [guest, setGuestValue] = useState('');
+  
+  return <div className='rsvp'>
+    <video autoPlay muted loop className='background-video'>
+      <source src={Background} type='video/mp4' />
+    </video>
+    <div className='container'>
+
+
+      <div className='photo'>
+        <img className="img-1" src={Image}></img>
+        <img className="img-2" src={Image}></img>
+      </div>
+      <div className='info'>
+      <h1>RSVP</h1>
+        <div className='verify'>
+          <p>To ensure we have everything prepared for your arrival, we kindly request that you RSVP as soon as possible. Your response will help us plan the perfect celebration and make sure everyone has a wonderful time.</p>
+          <input type='text' value={invitation}  placeholder='Insert Invitation Code'/>
+          <button>Submit</button>
+        </div>
+        <div className='sign-in'>
+          <div className='next-to-each-other'>
+            <input type='text' value={guest} placeholder='Name:'/>
+            <input type='text' value={guest} placeholder='Name:'/>
+          </div>
+          <input type='text' value={guest} placeholder='Name:'/>
+          <input type='text' value={guest} placeholder='Name:'/>
+          <button>Submit</button>
+        </div>
+      </div>
+    </div>
+
+  </div>;
 };
 
 export default RSVP;
